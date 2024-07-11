9f51d0ce744fd27f947e0bf87fead63dd17cc1ed
AzeemKhan27 <azeemdeveloper27@gmail.com>
2024-07-11 19:28:04 +0530

    delete useless files

--- ChatBotSupport/server/routes/chatRoutes.js
+++
@@ -1,9 +0,0 @@
-// import express from 'express';
-// import chatController from '../controllers/chatController';
-
-// const router = express.Router();
-
-// router.post('/chat', chatController.submitUserSuggestion);
-// router.get('/chat', chatController.generateBotResponse);
-
-// export default router;
