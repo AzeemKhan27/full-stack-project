25fd2fa8dfc40305839431d2b98b02de28c422c4
AzeemKhan27 <azeemdeveloper27@gmail.com>
2024-07-22 18:08:50 +0530

    add middlwares

--- knock-the-door/server/src/app.js
+++ knock-the-door/server/src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';    
 import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
 
 const app = express();
 
@@ -14,8 +15,8 @@ app.use(express.json({limit : "16kb"}));                         // we are allow
 app.use(express.urlencoded({extended : true, limit: "16kb"}));   //this middleware allows data from URL.
 app.use(express.static("public"))                                //this middleware allows to store files/images, and giving access to all.
 app.use(cookieParser());     
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));     
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));     
 
 //Routes Import
 import emailRoute from "./routes/mail.routes.js";
@@ -24,6 +25,14 @@ import userRoutes from "./routes/user.routes.js";
 //Routes Declaration
 app.use("/api/v1/mails",emailRoute);
 app.use("/api/v1/users",userRoutes);
+
+//debug code 
+app.use((req, res, next) => {
+    console.log("Incoming Request: ", req.method, req.url);
+    console.log("Headers: ", req.headers);
+    console.log("Body: ", req.body);
+    next();
+});
 
 
 export { app }
