b65c7632bedda03b929b6bd3406cfc9c1be5f38a
AzeemKhan27 <azeemdeveloper27@gmail.com>
2024-07-22 18:14:40 +0530

    correction in routes middleware

--- knock-the-door/server/src/routes/mail.routes.js
+++ knock-the-door/server/src/routes/mail.routes.js
@@ -2,11 +2,12 @@ import { Router } from "express";
 import mailController from '../controllers/mail.controller.js';
 import { scheduleMail, cancelMail, listScheduledMails } from '../controllers/mail.controller.js';
 // import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.route('/schedule-mail').post(mailController.uploadFile,scheduleMail); //uploadFile,scheduleMail
-router.route('/cancel-mail').post(cancelMail);
-router.route('/list-scheduled-mails').get(listScheduledMails);
+router.route('/schedule-mail').post(mailController.uploadFile,verifyJWT,scheduleMail); //uploadFile,scheduleMail
+router.route('/cancel-mail').post(verifyJWT,cancelMail);
+router.route('/list-scheduled-mails').get(verifyJWT,listScheduledMails);
 
 export default router;
