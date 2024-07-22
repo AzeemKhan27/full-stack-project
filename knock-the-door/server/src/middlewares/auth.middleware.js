099aa481f1381a86ce7375a795d92cd07b88ba9c
AzeemKhan27 <azeemdeveloper27@gmail.com>
2024-07-22 18:12:42 +0530

    debug and improve responses

--- knock-the-door/server/src/middlewares/auth.middleware.js
+++ knock-the-door/server/src/middlewares/auth.middleware.js
@@ -3,13 +3,13 @@ import { ApiError } from "../utils/ApiError.js";
 import JWT from "jsonwebtoken";
 import { User } from "../models/user.model.js";
 
-export const verifyJWT = asyncHandler(async(req, _, next) => {
+export const verifyJWT = asyncHandler(async(req, res, next) => {
     try {
        
         const token = req.cookies.accessToken || req.header("Authorization")?.replace("Bearer ", "");
 
         if(!token){
-            throw new ApiError(401,"Unauthorized request")
+           return res.status(401).json(new ApiError(401,token,"Unauthorized request || invalid token"))
         }
     
         const decodedToken = await JWT.verify(token,process.env.ACCESS_TOKEN_SECRET);
@@ -17,13 +17,12 @@ export const verifyJWT = asyncHandler(async(req, _, next) => {
         const user = await User.findById(decodedToken?._id)
                                .select("-password -refreshToken");
         if(!user){
-            throw new ApiError(401,"Invalid Access Token")
+            return res.status(401).json( new ApiError(401,user,"Invalid Access Token") )
         } 
         
         req.user = user;
         next()
     } catch (error) {
-        console.log("auth == 4 =>",error)
-        throw new ApiError("Invalid Access Token");
+        return res.status(500).json(new ApiError(500,error,"Invalid token || Expired Access Token."))
     }
 })
