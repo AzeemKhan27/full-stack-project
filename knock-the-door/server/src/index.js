3921073b23d2dd08019ec23705f9280988b898b8
AzeemKhan27 <azeemdeveloper27@gmail.com>
2024-07-22 19:58:40 +0530

    remove debug code and backup code

--- knock-the-door/server/src/index.js
+++ knock-the-door/server/src/index.js
@@ -2,9 +2,9 @@ import dotenv from 'dotenv';
 import { app } from "./app.js";
 // dotenv.config = ({path : './.env'});
 dotenv.config = ({path : '././.env'});
-console.log("process.env.PORT ==>", process.env.PORT)
 
 import connectDB from "./db/index.js";
+
 connectDB()
 .then(() => {
   app.listen(process.env.PORT || 8080, () => {
