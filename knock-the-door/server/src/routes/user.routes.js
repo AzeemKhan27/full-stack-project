eaad5e330a3e881f253bfc2b29cd34f2c3f4979e
AzeemKhan27 <azeemdeveloper27@gmail.com>
2024-07-25 17:47:16 +0530

    Backend : router

--- knock-the-door/server/src/routes/user.routes.js
+++ knock-the-door/server/src/routes/user.routes.js
@@ -1,26 +1,9 @@
 import { Router } from "express";
-import { registerAndSendOtp,verifyOtpAndLogin,registerUser, loginUser, logoutUser, refreshAccessToken, updateUserProfileImage, changePassword } from "../controllers/user.controller.js";
-import { upload } from "../middlewares/multer.middleware.js";
-import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { verifyOtp, loginAndSendOtp } from "../controllers/user.controller.js";
 
 const router = Router();
 
-router.route("/register").post(
-    upload.fields([
-        { name: "profileImage", maxCount: 1 }
-    ]),
-    registerUser
-    )
-
-router.route("/login").post(loginUser);
-
-router.route("/direct-register-sendotp").post(registerAndSendOtp,verifyJWT)
-router.route("/login-direct").post(verifyOtpAndLogin,verifyJWT);
-
-//secured routes    
-router.route("/logout").post(verifyJWT, logoutUser)  // "verifyJWT" this middleware verify token by Id.
-router.route("/refresh-token").post(refreshAccessToken) // here we are verifying the refresh token
-router.route("/change-password").post(verifyJWT,changePassword)
-router.route("/update-profile-image").patch(verifyJWT,upload.single("profileImage"),updateUserProfileImage);
+router.post('/verify-otp', verifyOtp); // Verify OTP
+router.post('/login-direct-and-register', loginAndSendOtp); // Login and send OTP
 
 export default router;
