a53746735be908a3bb051d44adc0fcd01e9756d9
AzeemKhan27git config --global user.name AzeemKhan27git config --global user.name AzeemKhan27 <azeemdeveloper27@gmail.com>
2024-11-29 23:57:09 +0530

    update App.js with student and client module

--- my-portfolio/client/src/App.jsx
+++ my-portfolio/client/src/App.jsx
@@ -1,24 +1,26 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import Routes
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import Header from './components/sections/Header';
 import Footer from './components/sections/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
+import ServiceMainPage from './pages/services/student/ServiceMainPage.jsx';
 import store from './redux/store';
-import ServiceMainPage from './pages/services/ServiceMainPage'; // Import ServiceMainPage
 
 function App() {
   return (
     <Provider store={store}>
       <Router>
         <Header />
-        <Routes> {/* Replace Router with Routes */}
-          <Route path="/" element={<Home />} /> {/* Use element prop instead of component */}
+        <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/services" element={<ServiceMainPage />} /> {/* Add Route */}
+          <Route path="/services/students" element={<ServiceMainPage />} />
+          <Route path="/services/clients" element={<ServiceMainPage />} />
+        
         </Routes>
         <Footer />
       </Router>
