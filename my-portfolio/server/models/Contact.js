40d8d767961b0c175ab8c622e9550b54b2f39cf3
AzeemKhan27git config --global user.name AzeemKhan27git config --global user.name AzeemKhan27 <azeemdeveloper27@gmail.com>
2024-11-29 10:54:30 +0530

    remove unwanted code

--- my-portfolio/server/models/Contact.js
+++ my-portfolio/server/models/Contact.js
@@ -12,15 +12,3 @@ const contactSchema = new mongoose.Schema({
 const Contact = mongoose.model('Contact', contactSchema);
 
 export default Contact;
-
-
-// models/contactModel.js
-// class Contact {
-//   constructor(name, email, message) {
-//     this.name = name;
-//     this.email = email;
-//     this.message = message;
-//   }
-// }
-
-// module.exports = Contact;
