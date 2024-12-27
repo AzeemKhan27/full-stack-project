9083c525fc9b23348e6643bd9fb3f10e05b5b815
AzeemKhan27 <azeemdeveloper27@gmail.com>
2024-12-27 15:58:02 +0530

    backend :updated routes and models

--- tech-assets-hub/my-portfolio/server/models/Course.js
+++ tech-assets-hub/my-portfolio/server/models/Course.js
@@ -1,17 +1,19 @@
-
 import mongoose from 'mongoose';
 
 const CourseSchema = new mongoose.Schema({
+  courseId: {type: String},
   title: { type: String, required: true },
   instructor: { type: String, required: true },
+  instructor_2: { type: String, default: "" }, // Optional field
   description: { type: String },
-  syllabus: { type: Array, required: true }, 
+  syllabus: { type: Array, required: true },
   price: { type: Number, required: true },
   duration: { type: String },
   timing: { type: String },
-  faq: { type: Array, required: true }, 
-  instructor_image: { type: String },
-  course_banner_image: { type: String },
+  faq: { type: Array, required: true },
+  instructor_image: { type: String, default: null }, // Optional image
+  instructor_image_2: { type: String, default: "" }, // Optional second image
+  course_banner_image: { type: String, default: null },
   createdAt: { type: Date, default: Date.now },
 });
 
--- tech-assets-hub/my-portfolio/server/routes/courseRoutes.js
+++ tech-assets-hub/my-portfolio/server/routes/courseRoutes.js
@@ -10,16 +10,22 @@ import  {
 } from '../controllers/courseController.js';
 
 const router = express.Router();
-// const upload = multer({ dest: 'uploads/' }); // Temporary file storage
 
-router.post('/', upload.fields([{ name: 'instructor_image', maxCount: 1 }, { name: 'course_banner_image', maxCount: 1 }]), createCourse); // Create a new course
+const uploadMiddleware = upload.fields([
+  { name: 'instructor_image', maxCount: 1 }, 
+  { name: 'course_banner_image', maxCount: 1 },
+  { name: 'instructor_image_2', maxCount: 1 },
+])
+
+router.post('/', 
+  uploadMiddleware, createCourse); // Create a new course
 
 // Route for searching courses
 router.get('/search', searchCoursesByTitle);
 
 router.get('/', getCourses); // Fetch all courses
 router.get('/:id', getCourseById); // Fetch a course by ID
-router.put('/:id', updateCourse); // Update a course
+router.put('/update', uploadMiddleware, updateCourse); // Update a course
 router.delete('/:id', deleteCourse); // Delete a course
 
 export default router;
