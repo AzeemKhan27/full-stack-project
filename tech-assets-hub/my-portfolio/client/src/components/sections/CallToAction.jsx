919574e31f4e6ab10b399e52acefa0195ca54bd0
AzeemKhan27 <azeemdeveloper27@gmail.com>
2024-12-31 12:29:32 +0530

    Frontend : Update and Enchance UI : Call to Action

--- tech-assets-hub/my-portfolio/client/src/components/sections/CallToAction.jsx
+++ tech-assets-hub/my-portfolio/client/src/components/sections/CallToAction.jsx
@@ -1,19 +1,16 @@
-// CallToAction.js
 import React from 'react';
 import './css/CallToAction.css'; // Import the CSS file
 
-const headline = "Join us today and take your project to the next level with our expert team.";
-
-const CallToAction = () => {
+const CallToAction = ({ headline }) => {
   return (
-    <div className="bg-blue-600 text-white p-8 rounded-lg text-center my-2">
-      <h2 className="text-2xl font-bold mb-2">Ready to Get Started?</h2>
+    <div className="bg-gradient-to-r from-blue-400 to-blue-600 text-white p-8 rounded-lg text-center my-8 shadow-lg">
+      <h2 className="text-3xl font-extrabold mb-2">Ready to Get Started?</h2>
       <p className="text-lg mb-4">
         {headline}
       </p>
       <a
         href="/contact"
-        className="bg-white text-blue-600 font-bold py-2 px-4 rounded transition duration-300 hover:bg-blue-500 hover:text-white"
+        className="bg-white text-blue-600 font-bold py-2 px-6 rounded-lg transition duration-300 hover:bg-blue-500 hover:text-white shadow-md transform hover:scale-105"
       >
         Contact Us
       </a>
