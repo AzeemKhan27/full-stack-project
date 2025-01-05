a9012431880608d4cfbe3449cb15425c1809cb7c
AzeemKhan27 <azeemdeveloper27@gmail.com>
2025-01-05 09:53:53 +0530

    Frontend : CSS : About Page

--- tech-assets-hub/my-portfolio/client/src/components/sections/AboutPage/OurStory.jsx
+++ tech-assets-hub/my-portfolio/client/src/components/sections/AboutPage/OurStory.jsx
@@ -1,17 +1,42 @@
 import React from 'react';
 
+// const OurStory = () => {
+//   return (
+//     <section className="py-16 bg-white-100">
+//       <div className="container mx-auto text-center">
+//         <h2 className="text-3xl font-bold mb-6">Our Story</h2>
+//         <p className="text-gray-600 mb-12">
+//           At "Tech Assets Hub", we began our journey with a vision to empower freelancers and businesses alike. 
+//           Our dedicated team of experienced professionals is passionate about providing high-quality services 
+//           and training that help individuals achieve their goals. We believe in the power of collaboration 
+//           and strive to create a community where talent meets opportunity. Join us as we continue to grow 
+//           and support each other in the ever-evolving freelance landscape.
+//         </p>
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default OurStory;
+
 const OurStory = () => {
   return (
-    <section className="py-16 bg-white-100">
+    <section className="py-16 bg-white">
       <div className="container mx-auto text-center">
-        <h2 className="text-3xl font-bold mb-6">Our Story</h2>
-        <p className="text-gray-600 mb-12">
+      
+      
+      <div className="bg-white p-8 rounded-lg shadow-lg mx-auto mb-8">
+      {/* <h2 className="text-3xl font-bold mb-6">Our Story</h2> */}
+      <h2 className="text-4xl font-extrabolds text-gray-800 mb-8">Our Story</h2> 
+      
+        <p className="text-lg text-gray-700">
           At "Tech Assets Hub", we began our journey with a vision to empower freelancers and businesses alike. 
           Our dedicated team of experienced professionals is passionate about providing high-quality services 
           and training that help individuals achieve their goals. We believe in the power of collaboration 
           and strive to create a community where talent meets opportunity. Join us as we continue to grow 
           and support each other in the ever-evolving freelance landscape.
         </p>
+      </div>
       </div>
     </section>
   );
