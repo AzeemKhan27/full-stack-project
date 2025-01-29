f0df24dbbf0b126e6c49995b7b0d34da5f26815a
AzeemKhan27 <azeemdeveloper27@gmail.com>
2025-01-29 20:43:45 +0530

    Frontend : component added : PrivacyPolicy

---
+++ tech-assets-hub/my-portfolio/client/src/pages/PrivacyPolicy.jsx
@@ -0,0 +1,13 @@
+// client/src/pages/PrivacyPolicy.jsx
+import React from 'react';
+
+const PrivacyPolicy = () => {
+  return (
+    <div>
+      <h1>Privacy Policy</h1>
+      <p>This is a placeholder for the privacy policy page.</p>
+    </div>
+  );
+};
+
+export default PrivacyPolicy;
