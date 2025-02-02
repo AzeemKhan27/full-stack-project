bcd5e5b6a31b787083a50ec604770e83f6a022f6
AzeemKhan27 <azeemdeveloper27@gmail.com>
2025-02-02 08:54:55 +0530

    Development : Frontend : TermsOfService.jsx and Privacy Policy

--- tech-assets-hub/my-portfolio/client/src/pages/PrivacyPolicy.jsx
+++ tech-assets-hub/my-portfolio/client/src/pages/PrivacyPolicy.jsx
@@ -1,11 +1,29 @@
-// client/src/pages/PrivacyPolicy.jsx
-import React from 'react';
+// src/pages/PrivacyPolicy.js
+
+import React, { useEffect, useState } from 'react';
+import apiService from '../services-api/apiService';
+import '../pages/css/PrivacyPolicy.css'; // Import the CSS file
 
 const PrivacyPolicy = () => {
+  const [policy, setPolicy] = useState('');
+
+  useEffect(() => {
+    const fetchPrivacyPolicy = async () => {
+      try {
+        const data = await apiService.getPrivacyPolicy();
+        setPolicy(data.policy);
+      } catch (error) {
+        console.error('Error fetching privacy policy:', error);
+      }
+    };
+
+    fetchPrivacyPolicy();
+  }, []);
+
   return (
-    <div>
+    <div className="privacy-policy">
       <h1>Privacy Policy</h1>
-      <p>This is a placeholder for the privacy policy page.</p>
+      <div dangerouslySetInnerHTML={{ __html: policy }} />
     </div>
   );
 };
--- tech-assets-hub/my-portfolio/client/src/pages/TermsOfService.jsx
+++ tech-assets-hub/my-portfolio/client/src/pages/TermsOfService.jsx
@@ -1,11 +1,29 @@
-// client/src/pages/TermsOfService.jsx
-import React from 'react';
+// src/pages/TermsOfService.js
+
+import React, { useEffect, useState } from 'react';
+import apiService from '../services-api/apiService';
+import '../pages/css/TermsOfService.css'; // Import the CSS file
 
 const TermsOfService = () => {
+  const [terms, setTerms] = useState('');
+
+  useEffect(() => {
+    const fetchTermsOfService = async () => {
+      try {
+        const data = await apiService.getTermsOfService();
+        setTerms(data.terms);
+      } catch (error) {
+        console.error('Error fetching terms of service:', error);
+      }
+    };
+
+    fetchTermsOfService();
+  }, []);
+
   return (
-    <div>
+    <div className="terms-of-service">
       <h1>Terms of Service</h1>
-      <p>This is a placeholder for the terms of service page.</p>
+      <div dangerouslySetInnerHTML={{ __html: terms }} />
     </div>
   );
 };
